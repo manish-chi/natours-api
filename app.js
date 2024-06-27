d1da334c46abb4b12cc2920bf429a73ac0f54b66
manish-chi <chitre.manish@gmail.com>
2024-06-27 12:38:18 +0530

    added get,patch,delete,post operations

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const fs = require("fs");
+const { profileEnd } = require("console");
 const app = express();
 
 app.use(express.json());
@@ -10,7 +11,28 @@ let tours = JSON.parse(
 );
 
 app.get("/api/v1/tours", (req, res) => {
-  return res.status(200).json(tours);
+  return res.status(200).json({
+    NumberOfTours: tours.length,
+    data: tours,
+  });
+});
+
+app.get("/api/v1/tours/:id", (req, res) => {
+  let { id } = req.params;
+  id = id * 1;
+  if (id >= tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Tour with given ID not found!",
+    });
+  } else {
+    let tour = tours.find((ele) => ele.id == id);
+    console.log(tour);
+    return res.status(200).json({
+      status: "success",
+      data: tour,
+    });
+  }
 });
 
 app.post("/api/v1/tours", (req, res) => {
@@ -18,21 +40,72 @@ app.post("/api/v1/tours", (req, res) => {
   let id = tours.length;
   tour = Object.assign(tour, { id: id });
   tours.push(tour);
+  writeToFile(tours, tour, res);
+});
 
-  try {
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.js`, tours);
+app.patch("/api/v1/tours/:id", (req, res) => {
+  let { id } = req.params;
+  id = id * 1;
+  let tour = tours[id];
 
+  let isPropValid = Object.keys(req.body).every((prop) =>
+    tour.hasOwnProperty(prop)
+  );
+
+  if (isPropValid) {
+    for (let property in req.body) {
+      if (tour.hasOwnProperty(property)) {
+        tour[property] = req.body[property];
+      }
+    }
     return res.status(200).json({
       status: "success",
+      message: "Tour have been updated",
       data: tour,
     });
-  } catch (err) {
+  } else {
     return res.status(500).json({
       status: "failed",
-      message: "Failed to add tour to tours-simple.js",
+      message: "Sorry, couldn't find the given property!",
     });
   }
 });
+
+app.delete("/api/v1/tours/:id", (req, res) => {
+  let id = req.params.id * 1;
+  let tourToDelete = tours[id];
+  if (tours.includes()) {
+    let updatedTours = tours.filter((tour) => {
+      return tour.id != tourToDelete.id;
+    });
+    writeToFile(updatedTours, tourToDelete, res);
+  } else {
+    return res.status(404).json({
+      status: "failed",
+      message: "Tour with ID not found!",
+    });
+  }
+});
+
+function writeToFile(tours, tour, res) {
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        return res.status(500).json({
+          status: "failed",
+          message: "Failed to modify tours-simple.json",
+        });
+      } else {
+        return res.status(200).json({
+          status: "success",
+          data: tour,
+        });
+      }
+    }
+  );
+}
 
 let port = 3000;
 
